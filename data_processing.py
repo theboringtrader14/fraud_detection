5ec40e1d6fbfc8f4b78c5b42328465080172593f
theboringtrader <theboringtrader14@gmail.com>
2026-07-05 21:33:01 +0530

    Revert "added a new function sum"
    This reverts commit 0b0ac08fe7957a1720baeff29ff4e4cb8c8eab50.

--- data_processing.py
+++ data_processing.py
@@ -1,11 +1,2 @@
 
 print('adding all data processing code related fraud detection')
-
-a = 5
-b = 4
-
-def sum(a, b):
-    c = a + b
-    return a
-
-print('added a new function sum which is incorrect')
